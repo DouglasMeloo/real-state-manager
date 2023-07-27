dfaf573e8e350f24a323a246534712320c96af07
Douglas Melo <douglasengmelo@gmail.com>
2023-07-27 12:22:08 +0100

    Add function for validating positive integers

--- run.py
+++ run.py
@@ -3,3 +3,14 @@ import sqlite3
 
 DATABASE = "./real_estate.db"
 TABLE = "property_listings"
+
+def validate_integer_input(prompt):
+    while True:
+        try: 
+            value = int(input(prompt)):
+            if value < 0:
+                raise ValueError("Please enter a positive integer.")
+            return value
+        except ValueError:
+            print("Invalid input. Please enter a positive integer.")
+            
